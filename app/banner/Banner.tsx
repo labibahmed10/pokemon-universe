eec04b36e2775c6be4087d9745071a4304c9b4c7
labibahmed10 <labib.ahmed.372@gmail.com>
2023-02-03 21:30:01 +0600

    showed the logo of the banner

--- app/banner/Banner.tsx
+++ app/banner/Banner.tsx
@@ -1,3 +1,4 @@
+import Logo from "@/components/Logo";
 import { url } from "inspector";
 import React from "react";
 import bgImg from "../../assets/Background.png";
@@ -5,11 +6,10 @@ import bgImg from "../../assets/Background.png";
 const Banner = () => {
    return (
       <section
-         className={`lg:h-screen lg:w-full bg-cover bg-center relative`}
+         className={`lg:h-screen bg-no-repeat w-full bg-cover bg-center relative`}
          style={{ backgroundImage: `url(${bgImg.src})` }}
       >
-
-         
+         <Logo />
       </section>
    );
 };
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import Image from "next/image";
+import React from "react";
+import logo from "../assets/Logo.png";
 
 const Logo = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+   return (
+      <Image
+         className="lg:mx-auto lg:pt-16 scale-100"
+         alt="logo image"
+         src={logo}
+      ></Image>
+   );
 };
 
 export default Logo;
