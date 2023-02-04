31364a403a3eaffad9fdeedfcef4d5e294333f28
labibahmed10 <labib.ahmed.372@gmail.com>
2023-02-04 17:28:55 +0600

    styling the card

--- app/card.tsx
+++ app/card.tsx
@@ -3,10 +3,9 @@ import Image from "next/image";
 import React from "react";
 
 const GET_SPECIFIC_POKEMON = gql`
-   query pokemon($name: String!) {
+   query ($name: String!) {
       pokemon(name: $name) {
          id
-         order
          types {
             type {
                name
@@ -16,9 +15,13 @@ const GET_SPECIFIC_POKEMON = gql`
    }
 `;
 
-
-
-
+interface GetSpecificPokemon {
+   pokemon: {
+      id: Number;
+      order: Number;
+      types: any;
+   };
+}
 
 type Pokemon = {
    id: Number;
@@ -27,25 +30,56 @@ type Pokemon = {
    dreamworld: String;
 };
 
+type Type = {
+   name: String;
+   slot: number;
+   type: any;
+};
+
+// type BaseName = {
+//    id: number;
+//    url: String;
+//    name: String;
+// };
+
 const Card = ({ pokemon }: { pokemon: Pokemon }) => {
-   const { data, variables, error } = useQuery(GET_SPECIFIC_POKEMON, {
+   const { data, variables, error } = useQuery<GetSpecificPokemon>(GET_SPECIFIC_POKEMON, {
       variables: {
          name: pokemon?.name,
       },
    });
 
-   console.log(data);
+   console.log(data?.pokemon?.types);
 
    return (
-      <div>
-         <Image
-            className="w-32 h-32"
-            width={100}
-            height={100}
-            alt="image of"
-            src={pokemon?.dreamworld as string}
-         ></Image>
-         <p>{pokemon?.name}</p>
+      <div className=" min-w-[220px] max-w-[260px] min-h-[280px] bg-white p-3 rounded-md hover:bg-water cursor-pointer group">
+         <div className="bg-slate-50 p-8 group-hover:bg-white rounded-lg relative">
+            <Image
+               className="lg:w-[160px] lg:h-[160px] mx-auto group-hover:scale-110 transition-all duration-500"
+               width={100}
+               height={100}
+               alt="image of"
+               src={pokemon?.dreamworld as string}
+            ></Image>
+
+            <p className="absolute top-2 left-5">
+               #{pokemon.id.toString().padStart(3, "0")}
+            </p>
+         </div>
+
+         <p className="text-base text-black group-hover:text-white font-semibold capitalize mt-3">
+            {pokemon?.name}
+         </p>
+
+         <div>
+            {data?.pokemon?.types?.map((type: Type, i: number) => {
+               return (
+                  <button className={`capitalize  bg-${type?.type?.name} btn`} key={i}>
+                     {type.type.name}
+                  </button>
+               );
+            })}
+         </div>
       </div>
    );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,19 @@ module.exports = {
       "./src/**/*.{js,ts,jsx,tsx}",
    ],
    theme: {
-      extend: {},
+      extend: {
+         colors: {
+            primary_blue: "#257bc4",
+            primary_yellow: "#FFCB05",
+            bug: "#9BCC50",
+            bug1: "#53a253",
+            poison: "#B97FC9",
+            fire: "#FC7C23",
+            psychic: "#F366B9",
+            water: "#4592C4",
+            gradient: "background: linear-gradient(180deg, #FC7C23 0%, #F366B9 100%);",
+         },
+      },
    },
    plugins: [],
 };
