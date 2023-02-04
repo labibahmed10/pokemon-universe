b8e4ad1587e4156dd0c5aec7bbfaf9164a38d6a7
labibahmed10 <labib.ahmed.372@gmail.com>
2023-02-05 01:48:56 +0600

    tried to add some sidebr animation

--- app/Universe.tsx
+++ app/Universe.tsx
@@ -1,18 +1,30 @@
 import Univ_blog from "./univ_blog";
-import image from "../assets/Brush.png";
+import brush from "../assets/Brush.png";
 import Image from "next/image";
+import imageUrl from "../assets/bg.png";
 
 const Universe = () => {
    return (
-      <section>
-         <div className="text-center relative flex justify-center mt-10">
-            <p className="text-5xl font-bold mt-5 text-primary_blue z-50">
-               Ash & Pikachu Arrive in <br /> Pokémon Universe
-            </p>
-            <Image className="absolute" src={image} alt=""></Image>
-         </div>
+      <section
+         className={`flex bg-contain bg-no-repeat`}
+         style={{
+            backgroundImage: `url(${imageUrl.src})`,
+         }}
+      >
+         {/* <Image className="" src={sidebar} alt="sidebar image"></Image> */}
 
-         <Univ_blog />
+         <section>
+            <div className="text-center relative flex justify-center mt-10">
+               <p className="text-5xl font-bold mt-5 text-primary_blue z-50">
+                  Ash & Pikachu Arrive in <br /> Pokémon Universe
+               </p>
+               <Image className="absolute" src={brush} alt=""></Image>
+            </div>
+
+            <Univ_blog />
+         </section>
+
+         {/* <Image src={sidebar} alt="sidebar image"></Image> */}
       </section>
    );
 };
